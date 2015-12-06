280c61fe66e18a3c0deabdbbd999b90bf9308d22
Anton Asabin <asannasa@mail.ru>
2015-12-06 23:48:02 +0500

    Test 6 Add Exe

--- HelloGit/main.cpp
+++ HelloGit/main.cpp
@@ -8,6 +8,8 @@ int main()
 	setlocale(LC_ALL, "Russian");
 	cout << "Hello, GitHub" << endl;
 	cout << "Test 4 to GitHub add EXE" << endl;
+	cout << "Test 5 to GitHub add EXE" << endl;
+	cout << "Test 6 to GitHub add EXE" << endl;
 	_getch();
 	return 0;
 }
