f725a1343bd842c34450172ebed93e9cc17dc3fb
Anton Asabin <asannasa@mail.ru>
2015-12-06 23:14:48 +0500

    init HelloGitHub

---
+++ HelloGit/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Hello, GitHub" << endl;
+	cout << "Привет, GitHub" << endl;
+
+	_getch();
+	return 0;
+}
